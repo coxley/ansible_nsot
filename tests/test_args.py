4348a27011469a62172d3ec7cd453985cbeaec2f
Codey Oxley <codey@yelp.com>
2015-09-14 12:25:49 -0700

    Fixed tests

--- tests/test_args.py
+++ tests/test_args.py
@@ -9,24 +9,24 @@ class TestParams:
             inventory.parse_args()
 
     def test_with_list(self):
-        sys.argv = '--list'.split()
+        sys.argv = 'ansible_nsot --list'.split()
         assert inventory.parse_args().list_
 
     def test_with_host(self):
-        sys.argv = '--host rtr.example.com'.split()
+        sys.argv = 'ansible_nsot --host rtr.example.com'.split()
         assert inventory.parse_args().host
 
         # Do not allow left out hostname if --host
         with pytest.raises(SystemExit):
-            sys.argv = '--host'.split()
+            sys.argv = 'ansible_nsot --host'.split()
             inventory.parse_args()
 
     def test_with_both(self):
         with pytest.raises(SystemExit):
-            sys.argv = '--list --host rtr.example.com'.split()
+            sys.argv = 'ansible_nsot --list --host rtr.example.com'.split()
             inventory.parse_args()
 
     def test_with_bad_option(self):
         with pytest.raises(SystemExit):
-            sys.argv = '--list --foo'.split()
+            sys.argv = 'ansible_nsot --list --foo'.split()
             inventory.parse_args()
