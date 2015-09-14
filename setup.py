4a65ac4e5e8d1c14c06fd4e9496033f6a2d3a862
Codey Oxley <codey@yelp.com>
2015-09-14 14:40:26 -0700

    Tests! Mocked out POST authenticate to test config properly

--- setup.py
+++ setup.py
@@ -62,6 +62,7 @@ setup(
             'pytest-flake8',
             'bumpversion',
             'ipython',
+            'requsets_mock',
         ],
     },
 
--- tests/test_client.py
+++ tests/test_client.py
@@ -1,21 +1,52 @@
 import os
+import requests
+import requests_mock
 from ansible_nsot.inventory import NSoTInventory
 
+client = None
 here = os.path.abspath(os.path.dirname(__file__))
 config_path = '%s/example.yaml' % here
 os.environ['NSOT_INVENTORY_CONFIG'] = config_path
-client = NSoTInventory()
-should_be = {
+base_url = 'http://localhost:8991/api'
+YAML_SHOULD_BE = {
     'routers': {
         'query': 'deviceType=ROUTER',
         'vars': {'a': 'b'}
     }
 }
 
+AUTH_RESPONSE = {
+    'status': 'ok',
+    'data': {'auth_token': 'bogus_token'}
+}
+
+
+@requests_mock.Mocker()
+class Server(object):
+    def __init__(self):
+        self.base_url = base_url
+
+    def test_authenticate(self, m):
+        uri = self.base_url + '/authenticate/'
+        m.register_uri('POST', uri, json=AUTH_RESPONSE)
+        return requests.post(uri).text
+
+
+def mock_auth():
+    with requests_mock.Mocker() as mock:
+        headers = {'Content-Type': 'application/json'}
+        auth_url = base_url + '/authenticate/'
+        mock.post(auth_url, json=AUTH_RESPONSE, headers=headers)
+        global client
+        client = NSoTInventory()
+
+
+mock_auth()
+
 
 class TestInventory:
     def test_groups(self):
-        assert client.groups == should_be.keys()
+        assert client.groups == YAML_SHOULD_BE.keys()
 
     def test_config(self):
-        assert client.config == should_be
+        assert client.config == YAML_SHOULD_BE
