07d981aa8ec1bd89827bda11257d11d64b11641c
Codey Oxley <codey@yelp.com>
2015-10-01 15:53:55 -0700

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ansible_nsot',
-    version='1.0.0',
+    version='1.0.1',
 
     description='Ansible NSoT Dynamic Inventory',
     long_description=long_description,
